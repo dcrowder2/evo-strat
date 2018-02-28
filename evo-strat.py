f70fb5b3b1c50aeae71954745f52fadfdbd2b8ca
Henry Thomas <Henry.BG2md@gmail.com>
2018-02-28 14:38:26 -0900

    Got it started

--- evo-strat.py
+++ evo-strat.py
@@ -1,1 +1,25 @@
+#Evolutionary Computing A412
+#Project 2: Evolution Strategies
+#Mar 9, 2018
+#Henry Thomas - htthomas@alaska.edu
+#Dakota Crowder - dcrowder2@alaska.edu
 
+import math as m
+
+#where -3 <= x1 <= 12 and 4 <= x2 <= 6
+function = lambda x1, x2: 21.5 + x1*m.sin(4*m.pi*x1) + x2*m.sin(20*m.pi*x2)
+
+def main():
+    number_of_parents = 3
+    number_of_offspring = 21
+    mutation_step_size = 1
+    termination_count = 10000
+    
+    n = 1 #step_sizes
+    overall_learning_rate = 1 / m.sqrt(2*n)
+    coordinate_learning_rate = 1 /  m.sqrt(2*m.sqrt(n))
+    
+    print function(6, 5)
+
+if __name__ == "__main__":
+    main()
